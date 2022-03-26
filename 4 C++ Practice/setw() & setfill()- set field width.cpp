a4b371d68ae75078cf77146be0f7b3903ceed6f6
MamunAkand <mamun.crick25@gmail.com>
2022-03-27 01:52:22 +0600

    Change

--- 4 C++ Practice/setw() & setfill()- set field width.cpp
+++
@@ -1,9 +0,0 @@
-#include<bits/stdc++.h>
-using namespace std;
-
-int main(){
-    int n = +564;
-    cout << setw(5) << setfill('0') << right << n << endl;
-    cout << setw(4) << setfill('*') << left << n << endl;
-    return 0;
-}
