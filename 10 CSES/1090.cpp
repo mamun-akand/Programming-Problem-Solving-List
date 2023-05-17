78c6090548dae67f88ae649ce399e7351f7d55a6
MamunAkand <mamun.crick25@gmail.com>
2023-05-18 03:03:30 +0600

    Here i do again

---
+++ 10 CSES/1090.cpp
@@ -0,0 +1,59 @@
+// URL: https://cses.fi/problemset/task/1090
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//=================================================================
+//TYPES
+#define   ll   long long int
+#define   ld 	long double	
+#define   vll	vector <ll>	
+
+//FUNCTIONS
+#define   FAST    ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define   all(v)  v.begin(),v.end()
+#define   setp(n) fixed << setprecision(n)
+#define   endl    "\n"
+#define   ln      cout<<"\n";
+#define   pb      push_back
+#define   eb      emplace_back
+#define   rn      return 0;
+
+#define   pa      cout<<ans<<endl
+#define   py      cout<<"YES"<<endl
+#define   pn      cout<<"NO"<<endl
+
+#define   Test    ll tc; cin>>tc; cin.ignore(); f1(t,tc)
+#define   cs      cout << "Case " << t << ": ";
+
+#define   f0(i,e)   for(ll i=0; i<e; i++)
+#define   f1(i,e)   for(ll i=1; i<=e; i++)
+#define   SpicyWings  return 0;
+
+#define   vp(v)   for(auto it:v){cout << it <<" ";}
+#define   sp(s)   while(!s.empty()){cout<<s.front() <<" ";s.pop();}
+//=================================================================
+
+int main(){
+   FAST;
+    
+   ll n, x; cin>>n>>x;
+   vll v(n); for(auto &it:v) cin>>it;
+   
+   sort(all(v));
+   
+   ll L=0, R=n-1, cnt=0;
+   while(L<=R){
+      if(v[L]+v[R] <= x){
+         cnt++; L++; R--;
+      }
+      else{
+         cnt++;
+         R--;
+      }
+   }
+   
+   cout << cnt << endl;
+   
+   SpicyWings;
+}
