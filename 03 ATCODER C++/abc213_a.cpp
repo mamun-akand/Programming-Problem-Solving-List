fa70c97b9694145cf876aec244327e7657317cf8
mamun-akand <mamun.crick25@gmail.com>
2024-09-10 06:17:20 +0600

    Programming

---
+++ 03 ATCODER C++/abc213_a.cpp
@@ -0,0 +1,56 @@
+// URL: https://vjudge.net/problem/atcoder-abc213_a
+#include<bits/stdc++.h>
+using namespace std;
+
+//====================================================================
+//TYPES
+#define   ll    long long int
+#define   ld    long double	
+#define   vll   vector <ll>	
+//FUNCTIONS
+#define   FAST    ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define   all(v)  v.begin(),v.end()
+#define   setp(n) fixed << setprecision(n)
+//#define   endl    '\n'
+#define   debug(x)   cout << #x << " : " << x << endl;
+#define   SpicyWings return 0;
+ 
+#define   ln      cout<<'\n';
+#define   pb      push_back
+#define   py      cout<<"YES"<<endl
+#define   pn      cout<<"NO"<<endl
+ 
+#define   Test    ll tc; cin>>tc; for(ll t=1; t<=tc; t++)
+#define   cs      cout << "Case " << t << ": ";
+#define   vp(v)   for(auto it:v){cout << it <<' ';}ln;
+//=====================================================================
+
+int main(){
+	FAST;
+   
+	ll a, b; cin>>a>>b;
+	cout << (a^b) << endl;
+   
+	SpicyWings;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
