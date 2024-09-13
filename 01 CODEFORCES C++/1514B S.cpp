8aa14b12471ef65ffd5266489268bc231def9e9e
mamun-akand <mamun.crick25@gmail.com>
2024-09-13 16:27:08 +0600

    Programming

---
+++ 01 CODEFORCES C++/1514B S.cpp
@@ -0,0 +1,63 @@
+// URL: https://vjudge.net/contest/579773#problem/H
+
+#include<bits/stdc++.h>
+using namespace std;
+
+//====================================================================
+//TYPES
+#define   ll    long long int
+#define   ld    long double	
+#define   vll   vector <ll>	
+//FUNCTIONS
+#define   FAST    ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define   all(v)  v.begin(),v.end()
+#define   setp(n) fixed << setprecision(n)
+//#define   endl    '\n'
+#define   debug(x)   cout << #x << " : " << x << endl;
+#define   SpicyWings return 0;
+ 
+#define   ln      cout<<'\n';
+#define   pb      push_back
+#define   py      cout<<"YES"<<endl
+#define   pn      cout<<"NO"<<endl
+ 
+#define   Test    ll tc; cin>>tc; for(ll t=1; t<=tc; t++)
+#define   cs      cout << "Case " << t << ": ";
+#define   vp(v)   for(auto it:v){cout << it <<' ';}ln;
+//=====================================================================
+const ll mod = 1e9 + 7;
+int main(){
+	FAST;
+   
+	Test{
+		ll n, k; cin>>n>>k;
+		ll ans=1;
+		for(ll i=1; i<=k; i++){
+			ans = (ans * n) % mod;
+		}
+		cout << ans << endl;
+	}
+   
+	SpicyWings;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
